612fe25f06bfc96eae56eff7785328db6052f0cd
K-shir0 <kakishiro2190394@gmail.com>
2022-09-28 13:45:57 +0900

    Initial Commit

---
+++ resteye/resteyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  resteyeApp.swift
+//  resteye
+//
+//  Created by K-shiro on 2022/09/28.
+//
+
+import SwiftUI
+
+@main
+struct resteyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
